4122643e002d24cf616dcb43745648c1f23e7e25
Dries <Dries.Ruttens@student.uhasselt.be>
2023-03-28 19:09:24 +0200

    Loggin button werkt

--- app/src/main/java/com/example/appdriesenmauro/UserActivity.kt
+++ app/src/main/java/com/example/appdriesenmauro/UserActivity.kt
@@ -1,9 +1,24 @@
 package com.example.appdriesenmauro
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.appdriesenmauro.databinding.ActivityLoginBinding
 
 
-class UserActivity {
+class UserActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
