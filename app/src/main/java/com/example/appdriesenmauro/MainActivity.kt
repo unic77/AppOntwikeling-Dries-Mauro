2f885a6a2efd1302e8573f5b001a4ef705495d0f
unic77 <112702446+unic77@users.noreply.github.com>
2023-03-29 16:54:29 +0200

    sandwitch aan menu fix

--- app/src/main/java/com/example/appdriesenmauro/MainActivity.kt
+++ app/src/main/java/com/example/appdriesenmauro/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import com.example.appdriesenmauro.databinding.ActivityMainBinding
@@ -36,12 +37,14 @@ class MainActivity : AppCompatActivity() {
         menuBarToggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open_menu, R.string.close_menu)
         binding.drawerLayout.addDrawerListener(menuBarToggle)
 
+
         menuBarToggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.navView.setNavigationItemSelectedListener {
             when(it.itemId){
+                R.id.btnLogOut -> goToProfile()
                 R.id.mnuHome -> goToHome()
                 R.id.mnuFavourites -> goToFavourites()
                 R.id.mnuAddActivity -> goTOAddActivity()
@@ -55,19 +58,23 @@ class MainActivity : AppCompatActivity() {
     private fun goToProfile() {
         var snak = Snackbar.make(binding.root, "dries is gay", Snackbar.LENGTH_LONG)
         snak.show()
+        binding.drawerLayout.closeDrawer(Gravity.LEFT)
     }
 
     private fun goToHome() {
+        binding.drawerLayout.closeDrawer(Gravity.LEFT)
         activityFragment.switchFiewToHome()
     }
 
     private fun gotToSupport(){
+        binding.drawerLayout.closeDrawer(Gravity.LEFT)
         var snak = Snackbar.make(binding.root, "sopport not online", Snackbar.LENGTH_LONG)
         snak.show()
     }
 
     private fun goToFavourites(){
         activityFragment.switchFiewToFavorite()
+        binding.drawerLayout.closeDrawer(Gravity.LEFT)
     }
 
     private fun goTOAddActivity(){
@@ -85,5 +92,13 @@ class MainActivity : AppCompatActivity() {
             addToBackStack("Fragment_${fragment.id}")
             commit()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // we need to do this to respond correctly to clicks on menu items, otherwise it won't be caught
+        if(menuBarToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
